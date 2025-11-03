a4962265899caa0de41b165a78958079e7d30896
HiAdriel <adriel_tjahja_34rpl@student.smktelkom-mlg.sch.id>
2025-11-03 11:14:30 +0700

    Add files via upload

---
+++ CekGanjilGenap.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CekGanjilGenap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah bilangan: ");
+        int angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println(angka + " adalah bilangan Genap.");
+        } else {
+            System.out.println(angka + " adalah bilangan Ganjil.");
+        }
+
+        input.close();
+    }
+}
